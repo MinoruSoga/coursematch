3b3d06f495342e8be010c14fe7b55889b1d24c56
Minoru <minoru.2021@icloud.com>
2019-02-22 15:58:01 +0800

    coursematch

--- index.php
+++ index.php
@@ -61,22 +61,6 @@
       <a href="signup.php" class="btn-get-started scrollto">Get Started</a>
     </div>
 
-    <div class="product-screens">
-
-      <div class="product-screen-1 wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="0.6s">
-        <img src="img/product-screen-1.png" alt="">
-      </div>
-
-      <div class="product-screen-2 wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="0.6s">
-        <img src="img/product-screen-2.png" alt="">
-      </div>
-
-      <div class="product-screen-3 wow fadeInUp" data-wow-duration="0.6s">
-        <img src="img/product-screen-3.png" alt="">
-      </div>
-
-    </div>
-
   </section><!-- #intro -->
 
 
